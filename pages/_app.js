f483c95670f8436953694e4a384497651693e8b2
Richard Zhang <notzree@gmail.com>
2022-11-22 21:11:21 -0500

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css'
+import { motion,AnimatePresence} from "framer-motion"
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
